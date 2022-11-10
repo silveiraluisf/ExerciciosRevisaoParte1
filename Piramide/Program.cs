b5c40bb580f7ce9c7c0fdf90aadc6b884d428b3a
silveiraluisf <silveiraluis88@gmail.com>
2022-11-10 16:28:29 -0300

    criação da classe piramide e sua propriedade

--- Piramide/Program.cs
+++ Piramide/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Teste");
+
+class Piramide
+{
+    private int N;
+
+    public int NdeAndares { 
+        get{
+            return N;
+                }
+        set
+        {
+            if (value > 0)
+                N = value;
+            else return null;
+        }
+    }
+
+}
