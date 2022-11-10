787f9db894f56e9aca64babf6a949da943d6175e
silveiraluisf <silveiraluis88@gmail.com>
2022-11-10 19:16:19 -0300

    função de construção da pirâmide feita

--- Piramide/Program.cs
+++ Piramide/Program.cs
@@ -1,21 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Teste");
+﻿using System;
+using System.Globalization;
 
 class Piramide
 {
-    private int N;
+ //   private int N;
 
-    public int NdeAndares { 
-        get{
-            return N;
-                }
-        set
-        {
-            if (value > 0)
-                N = value;
-            else return null;
+    //public int NdeAndares
+    //{
+      //  get
+        //{
+          //  return N;
+        //}
+        //set
+        //{
+          //  if (value > 0)
+            //    N = value;
+            //else Console.WriteLine("Favor inserir um valor válido maior que zero!");
+       // }
+   // }
+
+    public static void BuildPyramid(int N)
+    {
+            for (int i = 1; i <= N; i++)
+            {
+                // Laço de repeticao para os espacos
+                for (int j = 1; j <= (N - i); j++)
+                    Console.Write(" ");
+                // Aumenta o valor do numero
+                for (int Number = 1; Number <= i; Number++)
+                    Console.Write(Number);
+                // Diminui o valor no numero
+                for (int Number = (i - 1); Number >= 1; Number--)
+                    Console.Write(Number);
+                Console.WriteLine();
+            }
+            //Console.ReadLine();
         }
+
+    public static void Main()
+    {
+        BuildPyramid(4);
     }
 
 }
