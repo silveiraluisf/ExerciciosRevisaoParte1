581c8f5ffb1d369362504581340ebd962c7c2e5a
silveiraluisf <silveiraluis88@gmail.com>
2022-11-11 15:23:16 -0300

    criação do construtor da classe triangulo. Com referÊncia à classe vértice mas sem tratamento de exceções

---
+++ Triangulo/Program.cs
@@ -0,0 +1,31 @@
+﻿using static Calculations.Vertex; 
+
+class Triangle
+{
+    private Calculations.Vertex A;
+    private Calculations.Vertex B;
+    private Calculations.Vertex C;
+
+    public Triangle(Calculations.Vertex A, Calculations.Vertex B, Calculations.Vertex C) 
+    {
+        VertexA = A;
+        VertexB = B;
+        VertexC = C;
+    }
+
+    public Calculations.Vertex VertexA
+    {
+        get { return VertexA;}
+        set { VertexA = value; }
+    }
+    public Calculations.Vertex VertexB
+    {
+        get { return VertexB; }
+        set { VertexB = value; }
+    }
+    public Calculations.Vertex VertexC
+    {
+        get { return VertexC;}
+        set { VertexC = value; }
+    }
+}
--- Vertice/Program.cs
+++ Vertice/Program.cs
@@ -1,71 +1,61 @@
-﻿class Vertex {
+﻿namespace Calculations { 
+    public class Vertex {
 
-    private int X;
-    private int Y;
+        private int X;
+        private int Y;
 
-    public Vertex(int coordinateX, int coordinateY)
-    {
-        CoordinateX = coordinateX;
-        CoordinateY = coordinateY;
-    }
-
-    public int CoordinateX {
-        get
+        public Vertex(int coordinateX, int coordinateY)
         {
-            return X;
+            CoordinateX = coordinateX;
+            CoordinateY = coordinateY;
         }
-        private set 
-        {
-            X = value; 
-        } 
-    }
 
-    public int CoordinateY
-    {
-        get
-        {
-            return Y;
+        public int CoordinateX {
+            get { return X;}
+            private set { X = value; } 
         }
-        private set
+
+        public int CoordinateY
         {
-            Y = value;
+            get { return Y;}
+            private set { Y = value;}
         }
-    }
 
-    static public double Distance(Vertex A, Vertex B)
-    {
-        //formula: d (P, Q) = √ ((XQ - XP) ^ 2 + (YQ - YP) ^ 2)
-        double distante = Math.Sqrt(Math.Pow((A.CoordinateX - B.CoordinateX), 2) + Math.Pow((A.CoordinateY - B.CoordinateY), 2));    
-        Console.WriteLine($"A distância entre o vértice A e B é de {distante} unidade(s) de medida");
-        return distante;
-    }
+        static public double Distance(Vertex A, Vertex B)
+        {
+            //formula: d (P, Q) = √ ((XQ - XP) ^ 2 + (YQ - YP) ^ 2)
+            double distante = Math.Sqrt(Math.Pow((A.CoordinateX - B.CoordinateX), 2) + Math.Pow((A.CoordinateY - B.CoordinateY), 2));    
+            Console.WriteLine($"A distância entre o vértice A e B é de {distante} unidade(s) de medida");
+            return distante;
+        }
 
-    static public Vertex Move(Vertex A, Vertex B)
-    {
-        Console.WriteLine($"Posição atual do vértice = {A.CoordinateX} , {A.CoordinateY}");
-        A.CoordinateX = B.CoordinateX;
-        A.CoordinateY = B.CoordinateY;
-        Console.WriteLine("Moveu!");
-        Console.WriteLine($"Nova posiçã do vértice = {A.CoordinateX} , {A.CoordinateY}");
-        return A;
-    }
+        static public Vertex Move(Vertex A, Vertex B)
+        {
+            Console.WriteLine($"Posição atual do vértice = {A.CoordinateX} , {A.CoordinateY}");
+            A.CoordinateX = B.CoordinateX;
+            A.CoordinateY = B.CoordinateY;
+            Console.WriteLine("Moveu!");
+            Console.WriteLine($"Nova posiçã do vértice = {A.CoordinateX} , {A.CoordinateY}");
+            return A;
+        }
 
-    static public bool IsEqual(Vertex A, Vertex B)
-    {
-        Console.WriteLine(A == B);
-        return (A == B);
-    }
+        static public bool IsEqual(Vertex A, Vertex B)
+        {
+            Console.WriteLine(A == B);
+            return (A == B);
+        }
 
-    static void Main()
-    {
-        Console.WriteLine("Programa vertice");
-        Vertex vertexA = new(4, 5);
-        Vertex vertexB = new(2, 3);
-        Console.WriteLine("Função que calcula a distância entre dois vértices:");
-        Vertex.Distance(vertexA, vertexB);
-        Console.WriteLine("Função que move a posição de um vértice:");
-        Vertex.Move(vertexA, vertexB);
-        Console.WriteLine("Função que verifica se dois vértices são iguais:");
-        Vertex.IsEqual(vertexA, vertexB);
+        static void Main()
+        {
+            Console.WriteLine("Programa vertice");
+            Vertex vertexA = new(4, 5);
+            Vertex vertexB = new(2, 3);
+            Console.WriteLine("Função que calcula a distância entre dois vértices:");
+            Vertex.Distance(vertexA, vertexB);
+            Console.WriteLine("Função que move a posição de um vértice:");
+            Vertex.Move(vertexA, vertexB);
+            Console.WriteLine("Função que verifica se dois vértices são iguais:");
+            Vertex.IsEqual(vertexA, vertexB);
+        }
     }
 }
