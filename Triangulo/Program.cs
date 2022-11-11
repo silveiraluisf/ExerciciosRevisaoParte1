d63b9873a29426d98946e3527676b605d41341d0
silveiraluisf <silveiraluis88@gmail.com>
2022-11-11 18:55:36 -0300

    Versão com a propriedade do tipo de triângulo implementada como método

--- Triangulo/Program.cs
+++ Triangulo/Program.cs
@@ -1,70 +1,10 @@
-﻿using Vertice;
-
-namespace Triangulo
+﻿namespace Triangulo
 {
-    public class Triangle
-     {
-        private Vertex _A;
-        private Vertex _B;
-        private Vertex _C;
-
-        public Vertex VertexA
+    public class Program
+    {
+        static void Main()
         {
-            get { return _A; }
-            set { _A = value; }
+            
         }
-        public Vertex VertexB
-        {
-            get { return _B; }
-            set { _B = value; }
-        }
-        public Vertex VertexC
-        {
-            get { return _C; }
-            set { _C = value; }
-        }
-
-        public double D1
-        {
-            get { return Vertex.Distance(_A, _B); }
-        }
-
-        public double D2
-        {
-            get { return Vertex.Distance(_A, _C); }
-        }
-
-        public double D3
-        {
-            get { return Vertex.Distance(_B, _C); }
-        }
-
-        public double Perimeter
-        {
-            get { return D1 + D2 + D3; }    
-        }
-
-        public double Area
-        {
-            get { return Math.Sqrt(Perimeter / 2 * (Perimeter / 2 - D1) * (Perimeter / 2 - D2) * (Perimeter / 2 - D3)); }           
-        }
-
-        public Triangle(Vertex vertexA, Vertex vertexB, Vertex vertexC)
-        {
-            _A = vertexA;
-            _B = vertexB;
-            _C = vertexC;
-        }
-
-        static public bool IsEqual(Triangle a, Triangle b)
-            {
-                return a == b;
-            }
-
-        static public double CalculatePerimeter(Vertice.Vertex A, Vertice.Vertex B, Vertice.Vertex C)
-        {
-            double P = (Vertice.Vertex.Distance(A, B) + Vertice.Vertex.Distance(A, C) + Vertice.Vertex.Distance(B, C));
-            return P;
-        }
-     }
+    }
 }
---
+++ Triangulo/Triangle.cs
@@ -0,0 +1,87 @@
+﻿using Vertice;
+
+namespace Triangulo
+{
+    public class Triangle
+    {
+        private Vertex _A;
+        private Vertex _B;
+        private Vertex _C;
+        private TriangleTypes T;
+
+        public Vertex VertexA
+        {
+            get { return _A; }
+            set { _A = value; }
+        }
+        public Vertex VertexB
+        {
+            get { return _B; }
+            set { _B = value; }
+        }
+        public Vertex VertexC
+        {
+            get { return _C; }
+            set { _C = value; }
+        }
+
+        public double D1
+        {
+            get { return Vertex.Distance(_A, _B); }
+        }
+
+        public double D2
+        {
+            get { return Vertex.Distance(_A, _C); }
+        }
+
+        public double D3
+        {
+            get { return Vertex.Distance(_B, _C); }
+        }
+
+        public double Perimeter
+        {
+            get { return D1 + D2 + D3; }
+        }
+
+        public double Area
+        {
+            get { return Math.Sqrt(Perimeter / 2 * (Perimeter / 2 - D1) * (Perimeter / 2 - D2) * (Perimeter / 2 - D3)); }
+        }
+
+        public Enum TriangleType
+        {
+            get
+            {
+                return T;
+            }
+        }
+
+        public Triangle(Vertex vertexA, Vertex vertexB, Vertex vertexC)
+        {
+            _A = vertexA;
+            _B = vertexB;
+            _C = vertexC;
+        }
+
+        static public bool IsEqual(Triangle T1, Triangle T2)
+        {
+            return (T1.VertexA == T2.VertexA && T1.VertexB == T2.VertexB && T1.VertexC == T2.VertexC);
+        }
+
+        public void DefineTriangleType()
+        {
+            if (D1 == D2 && D2 == D3) { T = TriangleTypes.Equilatero; }
+            if (D1 == D2 || D2 == D3 || D3 == D1) { T = TriangleTypes.Isoseles; }
+            else T = TriangleTypes.Escaleno;      
+        }
+    }
+
+    public enum TriangleTypes
+    {
+        Equilatero,
+        Isoseles,
+        Escaleno,
+    }
+}
