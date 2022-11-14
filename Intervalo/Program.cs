3b9ec8f04c19a0c9df8c5ee3547f8bb79e9fc6f8
silveiraluisf <silveiraluis88@gmail.com>
2022-11-14 20:15:42 -0300

    Adicionados testes no programa do Intervalo

--- Intervalo/Program.cs
+++ Intervalo/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Intervalo
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Teste da classe intervalo:");
 
-Console.WriteLine("Hello, World!");
+            DateTime a = new DateTime(2008, 3, 1, 7, 0, 0);
+            DateTime b = new DateTime(2010, 3, 1, 7, 0, 0);
+
+            DateTime c = new DateTime(2006, 3, 1, 7, 0, 0);
+            DateTime d = new DateTime(2022, 3, 1, 7, 0, 0);
+
+            DateTime e = new DateTime(2006, 3, 1, 7, 0, 0);
+            DateTime f = new DateTime(2022, 3, 1, 7, 0, 0);
+
+            TimeInterval tempoA = new(a, b);
+            TimeInterval tempoB = new(c, d);
+            TimeInterval tempoC = new(e, f);
+
+            Console.WriteLine($"Intervalo 1: {tempoA.InicialDate} , {tempoA.FinalDate}");
+            Console.WriteLine($"Intervalo 2: {tempoB.InicialDate} , {tempoB.FinalDate}");
+            Console.WriteLine($"Tem interseção? {TimeInterval.HasInersection(tempoA, tempoB)}");
+            Console.WriteLine($"Os intervalos 1 e 2 são iguais? {TimeInterval.IsEqual(tempoA, tempoB)}");
+            Console.WriteLine("");
+
+            Console.WriteLine($"Intervalo 2: {tempoB.InicialDate} , {tempoB.FinalDate}");
+            Console.WriteLine($"Intervalo 3:  {tempoC.InicialDate} , {tempoC.FinalDate}");
+            Console.WriteLine($"Os intervalos 2 e 3 são iguais? {TimeInterval.IsEqual(tempoB, tempoC)}");
+            Console.WriteLine("");
+
+            Console.WriteLine($"A duração do intervalo 1 é: {tempoA.Duration}");
+            Console.WriteLine($"A duração do intervalo 2 é: {tempoB.Duration}");
+            Console.WriteLine($"A duração do intervalo 3 é: {tempoC.Duration}");
+        }
+    }
+}
