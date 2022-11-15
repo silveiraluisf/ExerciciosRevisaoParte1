26cd5be505556ac3bc3215aa6d10130d4d61a131
silveiraluisf <silveiraluis88@gmail.com>
2022-11-14 23:49:18 -0300

    Feitas as exceções para as propriedades nome, cpf, renda mensal, estado civil e numero de dependentes. Falta data de nascimento

--- Cliente/Client.cs
+++ Cliente/Client.cs
@@ -12,34 +12,128 @@
         public string Name
         {
             get { return _Name; }
-            private set { _Name = value; }
+            set { _Name = value; }
         }
         public long CPF
         {
             get { return _CPF; }
-            private set { _CPF = value; }
+            set { _CPF = value; }
         }
         public DateTime BornDate
         {
             get { return _BornDate; }
-            private set { _BornDate = value; }
+            set { _BornDate = value; }
         }
         public float MonthlyIncome
         {
             get { return _MonthlyIncome; }
-            private set { _MonthlyIncome = value; }
+            set { _MonthlyIncome = value; }
         }
         public char MaritalStatus
         {
             get { return _MaritalStatus; }
-            private set { _MaritalStatus = value; }
+            set { _MaritalStatus = value; }
         }
         public int NumberOfDependents
         {
             get { return _NumberOfDependents; }
-            private set { _NumberOfDependents = value; }
+            set { _NumberOfDependents = value; }
         }
 
+        public void NameValidate(Client C)
+        {
+            Console.WriteLine("Insira o nome do cliente: ");
+            C.Name = Console.ReadLine();
+            string name = C._Name;
+            switch (name.Length)
+            {
+                case 0:
+                    Console.WriteLine("O nome não pode ser vazio!");
+                    NameValidate(C);
+                    break;
+                case <5:
+                    Console.WriteLine("O nome deve possuir pelo menos 5 caracteres");
+                    NameValidate(C);
+                    break;
+                case >= 5:
+                    C.Name = _Name;
+                    break;
+            }           
+        }
 
+        public void CPFValidate(Client C)
+        {
+            Console.WriteLine("Insira o CPF: ");
+            string inputCPF = Console.ReadLine();
+            switch (inputCPF.Length)
+            {
+                case 11:
+                    C.CPF = Convert.ToInt64(inputCPF);
+                    C.CPF = _CPF;
+                    break;
+                default:
+                    Console.WriteLine("Favor insira um CPF válido (11 caracteres, apenas números).");
+                    CPFValidate(C);
+                    break;
+            }
+        }
+
+        public void BirthDateValidate(Client C)
+        {
+            Console.WriteLine("Insira a data de nascimento (DD/MM/AAAA):");
+            DateTime inputBirthDate = Convert.ToDateTime(Console.ReadLine());
+        }
+
+        public void IncomeValidate(Client C)
+        {
+            Console.WriteLine("Insira a renda mensal: ");
+            C.MonthlyIncome = (float)Convert.ToDouble(Console.ReadLine());
+            //if (C.MonthlyIncome < DateTimeKind(1,1,2002)) { }
+        }
+
+        public void StatusValidate(Client C)
+        {
+            Console.WriteLine("Insira o estado civil (C, S, V ou D): ");           
+            char inputStatus = Convert.ToChar(Console.ReadLine());
+            switch (Char.ToUpper(inputStatus))
+            {
+                case 'C':
+                    C.MaritalStatus = 'C';
+                    C.MaritalStatus = _MaritalStatus;
+                    break;
+                case 'S':
+                    C.MaritalStatus = 'S';
+                    C.MaritalStatus = _MaritalStatus;
+                    break;
+                case 'V':
+                    C.MaritalStatus = 'V';
+                    C.MaritalStatus = _MaritalStatus;
+                    break;
+                case 'D':
+                    C.MaritalStatus = 'D';
+                    C.MaritalStatus = _MaritalStatus;
+                    break;
+                default:
+                    Console.WriteLine("Favor insira um estado civil válido (C, S, V ou D).");
+                    StatusValidate(C);
+                    break;
+            }
+        }
+
+        public void DependentsValidate(Client C)
+        {
+            Console.WriteLine("Insira o número de dependentes (de 0 a 10): ");
+            C.NumberOfDependents = Convert.ToInt32(Console.ReadLine());
+            NumberOfDependents = C._NumberOfDependents;
+            if (NumberOfDependents >= 0 && NumberOfDependents <= 10)
+            {
+                C.NumberOfDependents = _NumberOfDependents;
+            }
+            else
+            {
+                Console.WriteLine("Favor insira um valor válido (entre 0 e 10).");
+                DependentsValidate(C);
+            }
+        }
     }
 }
--- Cliente/Program.cs
+++ Cliente/Program.cs
@@ -1,59 +1,46 @@
-﻿using System;
-
-namespace Cliente
+﻿namespace Cliente
 {
     public class Program
     {
         public static void Main()
         {
             Client clientA= new Client();
-            _ = clientA.Name;
-            _ = clientA.CPF;
-            _ = clientA.BornDate;
-            _ = clientA.MonthlyIncome;
-            _ = clientA.MaritalStatus;
-            _ = clientA.NumberOfDependents;
+           
 
             Console.WriteLine("---- BEM-VINDO AO SISTEMA DE CADASTRO ----");
             Console.WriteLine("");
-
-            Console.WriteLine("Insira o nome do cliente: ");
-            string name = Console.ReadLine();
-            Console.WriteLine($"O nome {name} foi cadastrado.");
+        
+            clientA.NameValidate(clientA);
+            Console.WriteLine($"O nome {clientA.Name} foi cadastrado.");
             Console.WriteLine("");
 
-            Console.WriteLine("Insira o CPF: ");
-            long cpf = Convert.ToInt64(Console.ReadLine());
-            Console.WriteLine($"O CPF {cpf} foi cadastrado.");
+            clientA.CPFValidate(clientA);
+            Console.WriteLine($"O CPF {clientA.CPF} foi cadastrado.");
             Console.WriteLine("");
 
-            Console.WriteLine("Insira a data de nascimento (DD/MM/AAAA): ");
-            DateTime born = Convert.ToDateTime(Console.ReadLine());
-            Console.WriteLine($"A data de nascimento {born} foi cadastrada.");
+            clientA.BirthDateValidate(clientA);  
+            Console.WriteLine($"A data de nascimento {clientA.BornDate} foi cadastrada.");
             Console.WriteLine("");
 
-            Console.WriteLine("Insira a renda mensal: ");
-            float income = (float) Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"A renda de {income:F2} $ por mês foi cadastrada.");
+            clientA.IncomeValidate(clientA);    
+            Console.WriteLine($"A renda de {clientA.MonthlyIncome:F2} $ por mês foi cadastrada.");
             Console.WriteLine("");
 
-            Console.WriteLine("Insira o estado civil (C, S, V ou D): ");
-            char status = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine($"O estado civil {status} $ foi cadastrado.");
+            clientA.StatusValidate(clientA);
+            Console.WriteLine($"O estado civil {clientA.MaritalStatus} foi cadastrado.");
             Console.WriteLine("");
 
-            Console.WriteLine("Insira o número de dependentes (de 0 a 10): ");
-            int dependents = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"O(s) {dependents} $ dependente(s) foi(ram) cadastrado(s).");
+            clientA.DependentsValidate(clientA);
+            Console.WriteLine($"O(s) {clientA.NumberOfDependents} dependente(s) foi(ram) cadastrado(s).");
             Console.WriteLine("");
 
             Console.WriteLine("---- RESUMO DO CASTRO ----");
-            Console.WriteLine($"Nome: {name}");
-            Console.WriteLine($"CPF: {cpf}");
-            Console.WriteLine($"Data de nascimento: {born}");
-            Console.WriteLine($"Renda mensal: {income}");
-            Console.WriteLine($"Estado civil: {status}");
-            Console.WriteLine($"Número de dependentes: {dependents}");
+            Console.WriteLine($"Nome: {clientA.Name}");
+            Console.WriteLine($"CPF: {clientA.CPFValidate}");
+            Console.WriteLine($"Data de nascimento: {clientA.BornDate}");
+            Console.WriteLine($"Renda mensal: {clientA.MonthlyIncome}");
+            Console.WriteLine($"Estado civil: {clientA.MaritalStatus}");
+            Console.WriteLine($"Número de dependentes: {clientA.NumberOfDependents}");
         }
     }
 }
